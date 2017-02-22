60aaf41c87e542c48e43ecf8e71b4093cf1e1537
Kory Brown <kjbrown123@ymail.com>
2017-02-22 12:33:14 -0800

    Added functionality to parseDefinition to take on ruleCommand. Still working out the bugs on storing data and retrieving.

--- Interface.cpp
+++ Interface.cpp
@@ -7,12 +7,17 @@ void Interface:: run()
 {
     
     
-    Helper::instance()->parseCommand("Fact Father(Roger,John)");
-    Helper::instance()->parseCommand("Fact Father(Roger,Albert)");
-    Helper::instance()->parseCommand("Fact Father(Allen,Margret)");
-//    Helper::instance()->parse("Rule GrandMother($X,$Y):- AND Mother($X,$Z) Mother($Z,$Y)");
+//    Helper::instance()->parseCommand("Fact Father(Roger,John)");
+//    Helper::instance()->parseCommand("Fact Father(Roger,Albert)");
+//    Helper::instance()->parseCommand("Fact Father(Allen,Margret)");
+//     Helper::instance()->parseCommand("Inference Father($X,$Y)");
     
-    Helper::instance()->parseCommand("Inference Father($X,$Y)");
+    Helper::instance()->parseCommand("Rule GrandMother($X,$Y):- AND Mother($X,$Z) Mother($Z,$Y)");
+    Helper::instance()->parseCommand("Inference GrandMother($X,$Y)");
+
+    
+    
+   
 
 
     
