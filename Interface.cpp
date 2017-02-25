abd300f66be14c2e529ce81d4ebf8c9471505715
Kory Brown <kjbrown123@ymail.com>
2017-02-24 18:06:06 -0800

    made commands up to standards

--- Interface.cpp
+++ Interface.cpp
@@ -7,20 +7,29 @@ void Interface:: run()
 {
     
     
-    Helper::instance()->parseCommand("Fact Father(Roger,John)");
-    Helper::instance()->parseCommand("Fact Father(Roger,Albert)");
-    Helper::instance()->parseCommand("Fact Father(Allen,Margret, Alliosn, Joe, Jim)");
-    //Helper::instance()->parseCommand("Inference Father($X,$Y)");
-    //Helper::instance()->parseCommand("Dump output.txt");
+    //    Helper::instance()->parseCommand("FACT Father(Roger,John)");
+    //    Helper::instance()->parseCommand("FACT Mother(Marry,John)");
+    //    Helper::instance()->parseCommand("FACT Father(Roger,Albert)");
+    //    Helper::instance()->parseCommand("FACT Mother(Marry,Albert)");
+    //    Helper::instance()->parseCommand("FACT Mother(Margret,Robert)");
+    //    Helper::instance()->parseCommand("FACT Mother(Marry,Albert)");
+    Helper::instance()->parseCommand("FACT Mother(Nancy,Margret)");
+    Helper::instance()->parseCommand("FACT Mother(Margret,Bob)");
     
     
-//    Helper::instance()->parseCommand("Rule GrandMother($X,$Y):- AND Mother($X,$Z) Mother($Z,$Y)");
-//    Helper::instance()->parseCommand("Inference GrandMother($X,$Y)");
-
+//    Helper::instance()->parseCommand("FACT Father(Roger,Albert)");
+//    Helper::instance()->parseCommand("FACT Father(Allen,Margret, Alliosn, Joe, Jim)");
+    //Helper::instance()->parseCommand("INFERENCE Father($X,$Y)");
+    //Helper::instance()->parseCommand("DUMP output.txt");
     
     
-   
-
-
+    Helper::instance()->parseCommand("RULE GrandMother($X,$Y):- AND Mother($X,$Z) Mother($Z,$Y)");
+        Helper::instance()->parseCommand("INFERENCE GrandMother($X,$Y)");
+    
+    
+    
+    
+    
+    
     
 }
