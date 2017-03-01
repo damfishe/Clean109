8d838f0612ac41b255179317c78b97f5186091b3
Kory Brown <kjbrown123@ymail.com>
2017-03-01 04:28:45 -0800

    Implemented retrieveRule to return vector<string> for usage outside of function

--- Helper.cpp
+++ Helper.cpp
@@ -363,7 +363,7 @@ vector<vector<string>> Helper:: retrieveFact(string key, string &param1, string 
 //
 // ===================================================================================
 
-void Helper:: retrieveRule(vector<string> params, string key)
+vector<string> Helper:: retrieveRule(vector<string> params, string key)
 {
     vector<string> query;
     string logicalOperater;
@@ -414,11 +414,12 @@ void Helper:: retrieveRule(vector<string> params, string key)
                  }
              });
     
-    
+    vector<string> nothing;
     if(logicalOperater=="AND")
-        andOperator(key, params, query);
+        return andOperator(key, params, query);
     else if (logicalOperater=="OR")
         orOperator(key, params, query);
+    return nothing;
 }
 
 // ===================================================================================
@@ -432,23 +433,23 @@ void Helper:: retrieveRule(vector<string> params, string key)
 //
 // ===================================================================================
 
-void Helper:: andOperator(string key, vector<string> keyParams, vector<string> query)
+vector<string> Helper:: andOperator(string key, vector<string> keyParams, vector<string> query)
 {
     vector<vector<string>> paramData; // holds parameters from each individual querey ie. Mother($x,$z) Mother($z,$y)
     vector<bool> paramCheck;
     vector<tuple<int,int,int,int>> paramIndex; // tuple<vectorIndex1,param,vectorIndex2,param>
+    vector<string> inferData; // holds the data to be returned
     
     
     for(int i=0; i < query.size(); i++)
         paramData.push_back(parseParams(query[i]));
     
-     vector<vector<string>> relationalData = retrieveFact(parseKey(query[0]),paramData[0][0],paramData[0][1]); // holds data from fact from each individual query in rule ie. Grandmother():- Mother() Mother()
+     vector<vector<string>> factData = retrieveFact(parseKey(query[0]),paramData[0][0],paramData[0][1]); // holds data from fact from each individual query in rule ie. Grandmother():- Mother() Mother()
     //this variable pulls the facts for the first rule target only; the preceding rule target may used the first rule target which will be handeled later
     
     // check parameters for correlation between rule targets
     for(int i=0; i < paramData.size()-1; i++) // controls the leftmost rule target  Mother($x,$z)<-leftmost Mother($z,$y)
         for(int param = 0; param < paramData[i].size(); param++) // iterates the leftmost rule target parameters
-            for(int param = 0; param < paramData[i].size(); param++)
                 for(int j=0; j < paramData[i+1].size(); j++) // iterates rule target paremeters to the right of leftmost
                 {
                     string a = paramData[i][param]; // used for testing
@@ -464,37 +465,43 @@ void Helper:: andOperator(string key, vector<string> keyParams, vector<string> q
     // grabs data from Fact based on parameters
     if (paramCheck.size() != paramData[0].size()) // checks to see if all parameters match, if they dont proceed
     {
-        vector<vector<string>> inferData = retrieveFact(parseKey(query[1]), relationalData[get<0>(paramIndex[0])][get<1>(paramIndex[0])], paramData[1][1]); // there should only be one vector contained so use index 0 to pull index to vector that has data
+        vector<vector<string>> relationalData = retrieveFact(parseKey(query[1]), factData[get<0>(paramIndex[0])][get<1>(paramIndex[0])], paramData[1][1]); // there should only be one vector contained so use index 0 to pull index to vector that has data
         cout << endl << key << " Inference: ";
         
         // this is where the logical operator logic happens
         vector<string> match;
         // loop through vectors one by one finding if theres a match if theres not then that means thats the data we want
-        for(int i=0; i < relationalData.size(); i++) // iterates through vector
-            for(int param = 0; param < relationalData[i].size(); param++) // iterate throguh vector's data
+        for(int i=0; i < factData.size(); i++) // iterates through vector
+            for(int param = 0; param < factData[i].size(); param++) // iterate throguh vector's data
                 for(int j=0; j < paramData[i].size(); j++) // iterate through inferData
                 {
                     // this code looks at parameter at a time comparing it to the results of inferData
                     // if the current parameter matches inferData then we dont want it ie. GrandMother($X,$Y):- Mother($X,$Z) Mother($Z,$Y)
                     // we dont want the $Z parameter so this code will leave you with $X, $Y
                     // the data only prints for now; and needs to be made more generic ie. nothing hard coded
-                    if (relationalData[i][param].compare(inferData[0][j]) == 0)
+                    if (factData[i][param].compare(relationalData[0][j]) == 0)
                     {
-                        match.push_back(inferData[0][j]);
+                        match.push_back(relationalData[0][j]);
                         break;
                     }
                     if (j == paramData[i].size()-1)
-                        cout << relationalData[i][param] << " ";
+                    {
+                        cout << factData[i][param] << " ";
+                        inferData.push_back(factData[i][param]);
+                    }
                 }
         
         
-        for(auto v: inferData) // loops through vector: v is the actual vector
+        for(auto v: relationalData) // loops through vector: v is the actual vector
             for(int i=0; i<v.size(); i++) // loops through data in vector
             { // doing it this way eliminate v[x][i]; just another way of looping
                 if (match[i].compare(v[i]) != 0)
+                {
                     cout << v[i] << " ";
+                    inferData.push_back(v[i]);
+
+                }
             }
-        cout << endl;
     }
     
     
@@ -526,6 +533,7 @@ void Helper:: andOperator(string key, vector<string> keyParams, vector<string> q
     //             });
     //
     cout << endl;
+    return inferData;
     
 }
 
